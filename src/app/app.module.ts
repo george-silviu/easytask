9774bd8b25aac198dace7b5261ff495c7ebd824c
unknown <costache.georgesilviu@yahoo.ro>
2025-07-30 23:26:08 +0300

    adding browser module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -7,6 +9,6 @@ import { UserComponent } from './user/user.component';
 @NgModule({
   declarations: [AppComponent], // here we add the module components
   bootstrap: [AppComponent], // here is specified the root component that Angular will use to start the application,
-  imports: [HeaderComponent, TasksComponent, UserComponent], // here we add the standalone components
+  imports: [BrowserModule, HeaderComponent, TasksComponent, UserComponent], // here we add the standalone components
 })
 export class AppModule {}
