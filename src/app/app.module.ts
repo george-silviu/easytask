bba37c81e31694f68f244fa929c96b5a6a0ac7ef
unknown <costache.georgesilviu@yahoo.ro>
2025-08-04 18:43:43 +0300

    created a sepparate module for tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
   declarations: [
     // here we can register non-standalone components
     AppComponent,
     HeaderComponent,
-    TasksComponent,
     UserComponent,
-    TaskComponent,
-    NewTaskComponent,
   ],
   bootstrap: [AppComponent], // here is specified the root component that Angular will use to start the application,
-  imports: [BrowserModule, FormsModule, SharedModule], // here we can register standalone components AND modules
+  imports: [BrowserModule, SharedModule, TasksModule], // here we can register standalone components AND modules
 })
 export class AppModule {}
